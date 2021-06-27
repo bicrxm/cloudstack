eec539e60ba0339e03b9086aad0e782ab268c35e
bicrxm <bickrombishsass@gmail.com>
2021-06-27 14:15:07 +0530

    changes

--- engine/schema/src/main/java/com/cloud/user/dao/SSHKeyPairDaoImpl.java
+++ engine/schema/src/main/java/com/cloud/user/dao/SSHKeyPairDaoImpl.java
@@ -66,12 +66,11 @@ public class SSHKeyPairDaoImpl extends GenericDaoBase<SSHKeyPairVO, Long> implem
 
     @Override
     public List<SSHKeyPairVO> findByNames(long accountId, long domainId, List<String> names) {
-        SearchCriteria<SSHKeyPairVO> sc = createSearchCriteria();
-        final Filter s_f = new Filter(SSHKeyPairVO.class,"name",false, null, null);
-        sc.addAnd("accountId", SearchCriteria.Op.EQ, accountId);
-        sc.addAnd("domainId", SearchCriteria.Op.EQ, domainId);
-        sc.addAnd("name", SearchCriteria.Op.EQ, names);
-        return search(sc, s_f);
+        List<SSHKeyPairVO> s_list = null;
+        for (String name : names) {
+            s_list.add(findByName(accountId, domainId, name));
+        }
+        return s_list;
     }
 
     @Override
